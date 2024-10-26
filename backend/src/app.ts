48e1b789e0ebacc3ec53d29d8f4f622b300c7979
Dmitriy Podorozhnykh <veseliy.koteika@yandex.ru>
2024-10-26 14:18:33 +0300

    Реализован функционал логирования

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,6 +5,7 @@ import productsRouter from './routes/product';
 import ordersRouter from './routes/order';
 import path from 'path';
 import errorHandler from './middlewares/error-handler';
+import { errorLogger, requestLogger } from './middlewares/logger';
 const cors = require('cors');
 
 const { PORT = 3000, DB_ADDRESS } = process.env;
@@ -14,10 +15,9 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 mongoose.connect(`${DB_ADDRESS}`);
-
+app.use(requestLogger);
 app.use('/product', productsRouter);
 app.use('/order', ordersRouter);
-
+app.use(errorLogger);
 app.use(errorHandler);
-
 app.listen(PORT, () => {console.log(`Сервер запущен на ${PORT} порту`)});
---
+++ backend/src/middlewares/logger.ts
@@ -0,0 +1,24 @@
+import path from 'path';
+const winston = require('winston');
+const expressWinston = require('express-winston');
+
+export const requestLogger = expressWinston.logger({
+  level: 'info',
+  transports: [
+    new winston.transports.File({
+      filename: path.join(__dirname, '../logs', 'request.log')
+    }),
+  ],
+  format: winston.format.json(),
+});
+
+// логгер ошибок
+export const errorLogger = expressWinston.errorLogger({
+  level: 'error',
+  transports: [
+    new winston.transports.File({
+      filename: path.join(__dirname, '../logs', 'error.log')
+    }),
+  ],
+  format: winston.format.json(),
+});
